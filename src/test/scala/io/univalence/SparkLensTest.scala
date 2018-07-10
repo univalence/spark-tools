d55944b46e8cba160b4885d8afec815ab3fec4f0
hwki77 <phil.hong77@gmail.com>
2018-07-10 16:33:58 +0200

    Spark Lens to change schema of deep structures

---
+++ src/test/scala/io/univalence/SparkLensTest.scala
@@ -0,0 +1,49 @@
+package io.univalence
+
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{ DataFrame, SparkSession }
+import org.apache.spark.sql.types.{ ArrayType, StringType }
+import org.apache.spark.sql.univalence.utils.SparkLens._
+import org.scalatest.FunSuite
+
+case class Toto(name: String, age: Int)
+
+case class Tata(toto: Toto)
+
+class SparkLensTest extends FunSuite {
+
+  val conf: SparkConf = new SparkConf()
+  conf.setAppName("yo")
+  conf.setMaster("local[*]")
+
+  implicit val ss: SparkSession = SparkSession.builder.config(conf).getOrCreate
+
+  import ss.implicits._
+
+  test("testLensRegExp") {
+    assert(lensRegExp(ss.createDataFrame(Seq(Toto("a", 1))))({
+      case ("name", StringType) ⇒ true
+      case _                    ⇒ false
+    }, { case (a: String, d) ⇒ Some(a.toUpperCase) }).as[Toto].first() == Toto("A", 1))
+  }
+
+  test("2") {
+    assert(lensRegExp(ss.createDataFrame(Seq(Tata(Toto("a", 1)))))({
+      case ("toto/age", _) ⇒ true
+      case _               ⇒ false
+    }, { case (a: Int, d) ⇒ Some(a + 1) }).as[Tata].first() == Tata(Toto("a", 2)))
+  }
+
+  ignore("3") {
+
+    val df: DataFrame = ???
+
+    lensRegExp(df)({
+      case (_, ArrayType(_, _)) ⇒ true
+      case _                    ⇒ false
+    }, (a, b) ⇒ if (a == null) Some(Nil) else None)
+  }
+
+}
+
