bb480e50f49cbef6df8f7617eed43f5198519050
Jonathan Winandy <jonathan.winandy@gmail.com>
2018-07-26 19:54:43 +0200

    implementation using monix only

--- src/main/scala/io/univalence/centrifuge/Executor.scala
+++ src/main/scala/io/univalence/centrifuge/Executor.scala
@@ -4,290 +4,110 @@ import java.util.UUID
 import java.util.concurrent.TimeUnit
 import java.util.function.Supplier
 
-import io.univalence.centrifuge.util.TaskLimiter
 import monix.eval.{Task, TaskCircuitBreaker}
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
 
 import scala.annotation.tailrec
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
-object CircuitOpenException extends Exception
+case class ExecutionSummary(nbFailure: Long)
 
-protected object ThreadLocalExecutor {
-  lazy val skipped: ExecutionResult[Nothing] = ExecutionResult(None, Some(ExecutionInfo(0, skipped = true, None)))
-}
-
-protected class ThreadLocalExecutor(var circuitClosed: Boolean = true, config: ExecutorConfig) {
-  import scala.concurrent.duration._
+object RetryDs {
 
 
-  private val circuitBreaker: Option[TaskCircuitBreaker] =  config.breakAfterNFailure.map(n => TaskCircuitBreaker(
-    maxFailures = n ,
-    resetTimeout = 600.seconds // 10 minutes by default
-    //exponentialBackoffFactor = 2,
-    //maxResetTimeout = 10.minutes
-  ))
+  private type LocalExecutionStatus = Boolean
+  private type ExecutionStat = Long
 
 
-
-  def retryBackoff[A](source: Task[A],
-                      maxRetries: Int, firstDelay: FiniteDuration): Task[A] = {
-
-    source.onErrorHandleWith {
-      case ex: Exception =>
-        if (maxRetries > 0)
-        // Recursive call, it's OK as Monix is stack-safe
-          retryBackoff(source, maxRetries-1, firstDelay*2)
-            .delayExecution(firstDelay)
-        else
-          Task.raiseError(ex)
-    }
-  }
-
-
-  private val aWeek: FiniteDuration = (60 * 60 * 24 * 7).seconds
-
-  //private var nbFailed: Int = 0
-
-  /*val limitRate: Option[TaskLimiter] = config.rateLimitPerSeconds.map(l => {
-    TaskLimiter(TimeUnit.SECONDS,l)
-  })*/
-
-  def endo[T](task:Task[T]):Task[T] = {
-
-    type Endo = Task[T] => Task[T]
-
-
-    List[Endo](
-      x => config.timeout.fold(x)(n => x.timeout(n.millisecond)),
-      config.rateLimitPerSeconds.fold[Endo](identity)((n:Int) => (x:Task[T]) => x.delayResult((1000 / n ).millisecond)),
-      x => config.backoff.fold(x.onErrorRestart(config.attempt - 1))(n => retryBackoff(x,config.attempt, n.millisecond)),
-      circuitBreaker.fold[Endo](identity)(t => x => t.protect(x))
-    ).foldLeft(task)((e,f) => f(e))
-  }
-
-  private var startTime: Long = _
-  final def run[T](f: () => Try[T]):ExecutionResult[T] = {
+  def retryDs[A, C, B](in: Dataset[A])
+                      (run: A => Try[C])
+                      (integrate: (A, Try[C]) => B)
+                      (nbGlobalAttemptMax: Int, circuitBreakerMaxFailure: Int = 10)
+                      (implicit encoderA: Encoder[A],
+                       encoderB: Encoder[B],
+                       encoderI: Encoder[(Option[A], LocalExecutionStatus, B)]): (Dataset[B], ExecutionSummary) = {
 
     import monix.execution.Scheduler.Implicits.global
-    import scala.concurrent.Await
-
-    import scala.concurrent.duration._
-    val t: Try[T] = Try{
-      Await.result(endo(Task(f().get)).runAsync,aWeek)
-    }
-
-    t match {
-      case Success(v) => ExecutionResult(Some(v),None)
-      case Failure(e) => ExecutionResult(None, Some(ExecutionInfo(1, skipped = false, Some(e.getMessage))))
-    }
-
+    Await.result(retryDsWithTask(in)(a => Task(run(a).get))(integrate)(nbGlobalAttemptMax = nbGlobalAttemptMax, circuitBreakerMaxFailure = Some(circuitBreakerMaxFailure)).runAsync, Duration.Inf)
   }
 
-}
+  private def toExecutionStat(les: LocalExecutionStatus): ExecutionStat = if (les) 0 else 1
 
-case class Executor protected[centrifuge] (config: ExecutorConfig) {
-  @transient private lazy val myThreadLocal: ThreadLocal[ThreadLocalExecutor] =
-    ThreadLocal.withInitial(
-      new Supplier[ThreadLocalExecutor] {
-        override def get(): ThreadLocalExecutor = new ThreadLocalExecutor(config = config)
+  private def addExecutionState(es1: ExecutionStat, es2: ExecutionStat): ExecutionStat = es1 + es2
+
+  private def initExecutionSummary(fes: ExecutionStat): ExecutionSummary = ExecutionSummary(fes)
+
+  private def localExecutionFromError(e: Throwable): LocalExecutionStatus = false
+
+  private val successExecution: LocalExecutionStatus = true
+
+  def retryDsWithTask[A, C, B](in: Dataset[A])
+                              (run: A => Task[C])
+                              (integrate: (A, Try[C]) => B)
+                              (nbGlobalAttemptMax: Int, circuitBreakerMaxFailure: Option[Int] = Option(10))
+                              (implicit
+                               encoderA: Encoder[A],
+                               encoderB: Encoder[B],
+                               encoderI: Encoder[(Option[A], LocalExecutionStatus, B)]
+                              ): Task[(Dataset[B], ExecutionSummary)] = {
+
+
+    type M = (Option[A], LocalExecutionStatus, B)
+
+    def newCircuitBreaker: Option[TaskCircuitBreaker] = circuitBreakerMaxFailure.map(n => TaskCircuitBreaker(n, Duration(1, TimeUnit.MINUTES)))
+
+
+    def aToM(a: A, endo:Task[C] => Task[C]): M = {
+      import monix.execution.Scheduler.Implicits.global
+      val task: Task[C] = endo(run(a))
+
+      val tried: Try[C] = Try(Await.result(task.runAsync, Duration.Inf))
+
+      tried match {
+        case Failure(e) => (Some(a), localExecutionFromError(e), integrate(a, tried))
+        case Success(_) => (None, successExecution, integrate(a, tried))
       }
-    )
+    }
 
-  def run[T](f: () ⇒ Try[T]): ExecutionResult[T] = {
-    myThreadLocal.get.run(f)
-  }
-}
-
-object ExecutionResult {
-
-  def success[O](out: O): ExecutionResult[O] = ExecutionResult(Option(out), None)
-
-  lazy val notExecuted: ExecutionResult[Nothing] = ExecutionResult(None, None)
-
-}
-
-case class ExecutorConfig(
-  name:                   String,
-  attempt:                Int,
-  backoff:                Option[Long] = None,
-  withExponentialBackoff: Boolean      = false,
-  rateLimitPerSeconds:    Option[Int]  = None,
-  breakAfterNFailure:     Option[Int]  = None,
-  timeout:                Option[Long] = None
-)
-
-object Executor {
-  def build: ExecutorBuilder = ExecutorBuildRep()
-}
-
-case class ExecutorBuildRep(
-  name:                       Option[String] = None,
-  attempt:                    Option[Int]    = None,
-  timeoutAttempt:             Option[Long]   = None,
-  backoff:                    Option[Long]   = None,
-  activateExponentialBackoff: Boolean        = false,
-  breakAfterNbFailure:        Option[Int]    = None,
-  maxNbCallPerSeconds:        Option[Int]    = None,
-  timeout:                    Option[Long]   = None
-) extends ExecutorBuilder {
-
-  def isValid: Boolean = name.isDefined
-
-  import scala.language.implicitConversions
-
-  private implicit def toOp[T](a: T): Option[T] = Option(a)
-
-  override def name(name: String): ExecutorBuilder = copy(name = name)
-
-  override def attempt(n: Int): ExecutorBuilder = copy(attempt = n)
-
-  //override def timeoutAttempt(milliseconds: Long): ExecutorBuilder = copy(timeoutAttempt = timeoutAttempt)
-
-  override def backoff(milliseconds: Long): ExecutorBuilder = copy(backoff = milliseconds)
-
-  override def withExponentialBackoff: ExecutorBuilder = copy(activateExponentialBackoff = true)
-
-  override def breakAfter(nbFailure: Int): ExecutorBuilder = copy(breakAfterNbFailure = nbFailure)
-
-  override def getOrCreate(): Executor = {
-    if (!isValid) throw new Exception("Invalid executor configuration")
-
-    Executor(ExecutorConfig(name = name.get, attempt.filter(_ > 1).getOrElse(1), breakAfterNFailure = breakAfterNbFailure, rateLimitPerSeconds = maxNbCallPerSeconds, backoff = backoff, timeout = timeout))
-  }
-
-  override def limitRate(nbCallPerSeconds: Int): ExecutorBuilder = copy(maxNbCallPerSeconds = nbCallPerSeconds)
-
-  override def timeout(milliseconds: Long): ExecutorBuilder = copy(timeout = milliseconds)
-}
-
-trait ExecutorBuilder {
-  def name(name: String = UUID.randomUUID().toString): ExecutorBuilder
-
-  def limitRate(nbCallPerSeconds: Int): ExecutorBuilder
-
-  def attempt(n: Int): ExecutorBuilder
-  def backoff(milliseconds: Long): ExecutorBuilder
-  def withExponentialBackoff: ExecutorBuilder
-
-  def breakAfter(nbFailure: Int): ExecutorBuilder
-
-  def timeout(milliseconds: Long): ExecutorBuilder
-
-  def getOrCreate(): Executor
-}
-
-case class Toto(name: String, age: Int)
-
-case class ExecutionResult[+T](out: Option[T], info: Option[ExecutionInfo]) {
-  def isSuccess: Boolean = out.isDefined
-
-  def toTry: Try[T] = out.map(Success.apply).getOrElse(info match {
-    case Some(i) if i.skipped ⇒ Failure(CircuitOpenException)
-    case Some(i)              ⇒ Failure(new Exception(i.lastError.getOrElse("???")))
-
-  })
-}
-
-case class ExecutionInfo(nbAttempt: Int, skipped: Boolean, lastError: Option[String])
-case class ExecMetrics(success: Long = 0, skipped: Long = 0, failed: Long = 0, totalAttempts: Long = 0) {
-
-  def isPureSuccess: Boolean = skipped == 0 && failed == 0
-
-  def add(execMetrics: ExecMetrics): ExecMetrics = ExecMetrics(
-    success = execMetrics.success + success,
-    skipped = execMetrics.skipped + skipped,
-    failed = execMetrics.failed + failed,
-    totalAttempts = execMetrics.totalAttempts + totalAttempts
-  )
-}
-
-object PrgSpark {
-
-  import scala.reflect.runtime.universe.TypeTag
-
-  def retryDs[A <: Product: TypeTag, B <: Product: TypeTag, CallRes <: Product: TypeTag](in: Dataset[A])(run: A ⇒ Try[CallRes])(integrate: (A, Try[CallRes]) ⇒ B)(
-    nbAttemptStageMax: Int,
-    executor:          Executor = Executor.build.name().breakAfter(20).getOrCreate()
-  ): Dataset[B] = {
-
-    @tailrec
-    def innerLoop(
-      ds:                Dataset[(A, ExecutionResult[CallRes], B)],
-      remainingAttempts: Int,
-      oldDs:             Option[Dataset[(A, ExecutionResult[CallRes], B)]]
-    ): Dataset[B] = {
-
-      import in.sparkSession.implicits._
-
-      if (remainingAttempts == 0) ds.map(_._3) else {
-        ds.persist()
-        val execMetrics: ExecMetrics = ds.map(x ⇒ {
-          val r: ExecutionResult[CallRes] = x._2
-          () match {
-            case _ if r.out.isDefined ⇒ ExecMetrics(success = 1)
-            case _ ⇒ r.info.get match {
-              case i if i.skipped ⇒ ExecMetrics(skipped = 1)
-              case i              ⇒ ExecMetrics(failed = 1, totalAttempts = i.nbAttempt)
-            }
-          }
-        }).reduce(_.add(_))
-
-        oldDs.map(_.unpersist())
-        if (execMetrics.isPureSuccess) ds.map(_._3) else {
-          //TODO : OptimByPartition
-          val newDs: Dataset[(A, ExecutionResult[CallRes], B)] = ds.map(t ⇒ {
-            val (a, e, b) = t
-            if (e.out.isEmpty) {
-              val ne: ExecutionResult[CallRes] = executor.run(() ⇒ run(a))
-              (a, ne, integrate(a, ne.toTry))
-            } else {
-              t
-            }
+    def loopTheLoop(mAndEs: (Dataset[M], ExecutionSummary), attemptRemaining: Int): Task[(Dataset[M], ExecutionSummary)] = {
+      if (mAndEs._2.nbFailure == 0 || attemptRemaining <= 0)
+        Task.pure(mAndEs)
+      else {
+        Task {
+          val (ds, _) = mAndEs
+          val newDs: Dataset[(Option[A], LocalExecutionStatus, B)] = ds.mapPartitions(iterator => {
+            val circuitBreaker = newCircuitBreaker
+            iterator.map({
+              case (Some(a), _, _) => aToM(a, x => circuitBreaker.fold(x)(_.protect(x)))
+              case x => x
+            })
           })
-
-          innerLoop(newDs, remainingAttempts - 1, Some(ds))
-        }
+          newDs.persist()
+          val es: ExecutionSummary = dsToEs(newDs)
+          ds.unpersist()
+          (newDs, es)
+        }.flatMap(x => loopTheLoop(x, attemptRemaining - 1))
       }
     }
 
-    import in.sparkSession.implicits._
+    def dsToEs(ds: Dataset[M]): ExecutionSummary = {
+      import ds.sparkSession.implicits._
+      initExecutionSummary(ds.map(_._2).rdd.map(toExecutionStat).reduce(addExecutionState))
+    }
 
-    val res = in.map(x ⇒ {
-      val e = executor.run(() ⇒ run(x))
-      (x, e, integrate(x, e.toTry))
-    })
+    Task({
+      val init: Dataset[M] = {
+        in.mapPartitions(iterator => {
+          val circuitBreaker = newCircuitBreaker
+          iterator.map(x => aToM(x,t => circuitBreaker.fold(t)(_.protect(t))))
+        })
+      }
 
-    res.persist()
-
-    innerLoop(res, nbAttemptStageMax, None)
-
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    val ss: SparkSession = ???
-
-    import ss.implicits._
-
-    val ds = ss.read.parquet("toto").as[Toto]
-
-    val res: Dataset[(Toto, Boolean)] = PrgSpark.retryDs(ds)(toto ⇒ Try {
-      /*
-
-      // wegbet ....
-
-       */
-
-      (1, 2)
-    })({
-      case (toto, Success(value)) ⇒ (toto.copy(age = toto.age * value._1 + value._2), true)
-      case (toto, Failure(_))     ⇒ (toto, false)
-
-    })(nbAttemptStageMax = 10)
+      init.persist()
+      (init, dsToEs(init))
+    }).flatMap(x => loopTheLoop(x, nbGlobalAttemptMax - 1)).map({ case (a, b) => (a.map(_._3), b) })
 
   }
 }
-
--- src/test/scala/io/univalence/centrifuge/ExecutorTest.scala
+++ src/test/scala/io/univalence/centrifuge/ExecutorTest.scala
@@ -1,98 +1,23 @@
 package io.univalence.centrifuge
 
+import io.univalence.Toto
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.FunSuite
+import monix.eval.Task
+import monix.execution.CancelableFuture
 
-import scala.util.{ Failure, Success, Try }
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success, Try}
 
 class ExecutorTest extends FunSuite {
 
   def error: Nothing = throw new Exception("run error")
 
-  test("breakafter") {
-
-    val executor: Executor = Executor.build.name().breakAfter(2).getOrCreate()
-
-    def s(e: ⇒ Any) = assert(executor.run(() ⇒ Try(e)).isSuccess)
-    def f(e: ⇒ Any) = assert(!executor.run(() ⇒ Try(e)).isSuccess)
-
-    s(1)
-    f(error)
-    s(2)
-    f(error)
-    f(error)
-    f(3)
-    f(4)
-  }
-
-  def failN[A](n: Int, a: A): () ⇒ Try[A] = {
-    new Function0[Try[A]] {
-      private var nbFailure: Int = n
-      override def apply(): Try[A] = {
-        if (nbFailure > 0) {
-          nbFailure = nbFailure - 1
-          Try { throw new Exception("fail")}
-        } else {
-          Try(a)
-        }
-      }
-    }
-  }
-
-  test("attempt") {
-
-    val executor: Executor = Executor.build.name().attempt(2).getOrCreate()
-
-    def s(e: ⇒ Any) = assert(executor.run(() ⇒ Try(e)).isSuccess)
-    def f(e: ⇒ Any) = assert(!executor.run(() ⇒ Try(e)).isSuccess)
-
-    //test failN
-    {
-      val f = failN(2, "value")
-      assert(f().isFailure)
-      assert(f().isFailure)
-      assert(f().isSuccess)
-    }
 
 
-      val r1: () ⇒ Try[String] = failN(1, "value")
-      s(r1().get)
 
-      val r2 = failN(2, "value")
-      f(r2().get)
-      s(r2().get)
-
-
-  }
-
-  test("limitRate") {
-    val executor: Executor = Executor.build.name().limitRate(100).getOrCreate()
-
-    def s(e: ⇒ Any) = assert(executor.run(() ⇒ Try(e)).isSuccess)
-    def f(e: ⇒ Any) = assert(!executor.run(() ⇒ Try(e)).isSuccess)
-
-    val start = System.currentTimeMillis()
-    (1 to 10).foreach(x ⇒ s(x))
-
-    assert(System.currentTimeMillis() - start >= 100)
-  }
-
-  test("backoff") {
-
-    val executor: Executor = Executor.build.name().backoff(10).attempt(100).getOrCreate()
-
-    def s(e: ⇒ Any) = assert(executor.run(() ⇒ Try(e)).isSuccess)
-    def f(e: ⇒ Any) = assert(!executor.run(() ⇒ Try(e)).isSuccess)
-
-    s(1)
-
-    val start = System.currentTimeMillis()
-    val r = failN(4, 1)
-    s(r().get)
-    assert(System.currentTimeMillis() - start >= 100)
-
-  }
 
   test("spark") {
 
@@ -107,19 +32,41 @@ class ExecutorTest extends FunSuite {
     val totoes = Seq(Toto("a", 1), Toto("b", 2))
     val ds = ss.createDataset(totoes)
 
-    assert(PrgSpark.retryDs(ds)(run = x ⇒ Try(2 -> 1))({ case (a, Success((2, 1))) ⇒ a })(nbAttemptStageMax = 2).collect().toSeq == totoes)
+    assert(RetryDs.retryDs(ds)(run = x ⇒ Try(2 -> 1))({ case (a, Success((2, 1))) ⇒ a })(nbGlobalAttemptMax = 1000)._1.collect().toSeq == totoes)
 
     val startDate: Long = System.currentTimeMillis()
 
     val wait = 5000
 
-    val res = PrgSpark.retryDs(ds)(
+    val res = RetryDs.retryDs(ds)(
       run = x ⇒ if (System.currentTimeMillis() - startDate > wait) Try(2 -> 1) else Failure(new Exception("too soon"))
-    )({ case (a, _) ⇒ a })(nbAttemptStageMax = 1000)
+    )({ case (a, _) ⇒ a })(1000)
 
-    assert(res.collect().toSeq == totoes)
+    assert(res._1.collect().toSeq == totoes)
 
     assert(System.currentTimeMillis() - startDate > wait)
   }
+
+
+  test("second implementation should work on pure function") {
+    val conf: SparkConf = new SparkConf()
+    conf.setAppName("yo")
+    conf.set("spark.sql.caseSensitive", "true")
+    conf.setMaster("local[2]")
+
+    implicit val ss: SparkSession = SparkSession.builder.config(conf).getOrCreate
+    import ss.implicits._
+
+    import monix.execution.Scheduler.Implicits.global
+
+    val t  =RetryDs.retryDsWithTask(ss.createDataset(Seq(1, 2, 3)))(x => Task(x + 1))({ case (a, Success(c)) => c })(1)
+
+
+    assert(Await.result(t.runAsync,Duration.Inf)._1.collect().toList == List(2,3,4))
+
+
+
+  }
+
 
 }
