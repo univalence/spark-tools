4188831fa744671ac880be72793a43fc924c928f
Jonathan Winandy <jonathan.winandy@gmail.com>
2019-04-09 17:13:42 +0200

    [spark-zio] impl zwrite

--- spark-zio/src/main/scala/SparkEnv.scala
+++ spark-zio/src/main/scala/SparkEnv.scala
@@ -3,19 +3,21 @@ package io.univalence.sparkzio
 import org.apache.spark.sql.{ DataFrame, Dataset, SparkSession }
 import scalaz.zio.{ DefaultRuntime, IO, Task, TaskR, ZIO }
 
+/*
 case class OptionSpark(
-  key: String   = "",
-  value: String = ""
-)
+                        key: String = "",
+                        value: String = ""
+                      )
+ */
 
-trait Write {
-  def option(key: String, value: String): Write
+class Write[T](ds: Dataset[T], options: Seq[(String, String)]) {
+  def option(key: String, value: String): Write[T] = new Write(ds, options :+ (key -> value))
 
-  def textFile(path: String): Task[Unit]
+  def text(path: String): Task[Unit] = Task.effect(ds.write.options(options.toMap).text(path))
 
-  def parquet(path: String): Task[Unit]
+  def parquet(path: String): Task[Unit] = Task.effect(ds.write.options(options.toMap).parquet(path))
 
-  def cache: Task[Unit]
+  def cache: Task[Unit] = Task.effect(ds.cache)
 }
 
 trait SparkEnv {
@@ -30,11 +32,12 @@ trait SparkEnv {
 
   def read: Read
 
-  def write[T](ds: Dataset[T]): Write
+  def write[T](ds: Dataset[T]): Write[T] = new Write(ds, Seq.empty)
 
   trait Query {
     def sql(query: String): Task[DataFrame]
   }
+
   def query: Query
 }
 
@@ -50,21 +53,10 @@ class SparkZIO(ss: SparkSession) extends SparkEnv {
     override def textFile(path: String): Task[DataFrame] =
       Task.effect(ss.read.textFile(path).toDF())
   }
+
   override def read: Read = readTrait
 
-  override def write[T](ds: Dataset[T]): Write = new Write {
-    override def option(key: String, value: String): Write =
-      ???
-
-    override def textFile(path: String): Task[Unit] =
-      Task.effect(ds.write.text(path))
-
-    override def parquet(path: String): Task[Unit] =
-      Task.effect(ds.write.parquet(path))
-
-    override def cache: Task[Unit] =
-      Task.effect(ds.cache())
-  }
+  override def write[T](ds: Dataset[T]): Write[T] = new Write(ds, Seq.empty)
 
   val queryTrait: Query = new Query {
     override def sql(query: String): Task[DataFrame] =
@@ -81,8 +73,9 @@ object SparkEnv {
     implicit class DsOps[T](ds: Dataset[T]) {
       def zcache: Task[Dataset[T]] = Task.effect(ds.cache)
 
-      def zwrite: Write = ???
+      def zwrite: Write[T] = new Write(ds, Seq.empty)
     }
+
   }
 
   type TaskS[X] = TaskR[SparkEnv, X]
@@ -102,7 +95,7 @@ object SparkTest {
 
     val programWrite: ZIO[SparkEnv, Throwable, DataFrame] = for {
       df <- sparkEnv.read.textFile("tata")
-      _  <- df.zwrite.textFile("tata3")
+      _  <- df.zwrite.text("tata3")
     } yield df
 
     val liveProgramWrite: IO[Throwable, DataFrame] = programWrite.provide(sparkEnv)
