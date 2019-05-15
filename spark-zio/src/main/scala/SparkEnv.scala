e1940c5ebf68d8d4a6080002668a617857ab029c
hwki <philippe@univalence.io>
2019-05-15 15:20:32 +0200

    [spark-zio] format + option

--- spark-zio/src/main/scala/SparkEnv.scala
+++ spark-zio/src/main/scala/SparkEnv.scala
@@ -1,16 +1,22 @@
 package io.univalence.sparkzio
 
-import org.apache.spark.sql.{ DataFrame, Dataset, SparkSession }
-import scalaz.zio.{ DefaultRuntime, IO, Task, TaskR, ZIO }
+import org.apache.spark.sql.{DataFrame, DataFrameReader, Dataset, SparkSession}
+import scalaz.zio.{Task, TaskR, ZIO}
 
-class Write[T](ds: Dataset[T], options: Seq[(String, String)]) {
-  def option(key: String, value: String): Write[T] = new Write(ds, options :+ (key -> value))
+case class Write[T](ds: Dataset[T], options: Seq[(String, String)], format: Option[String]) {
+  def option(key: String, value: String): Write[T] = this.copy(options = options :+ (key -> value))
 
-  def text(path: String): Task[Unit] = Task.effect(ds.write.options(options.toMap).text(path))
+  def text(path: String): Task[Unit] = Task(ds.write.options(options.toMap).text(path))
 
-  def parquet(path: String): Task[Unit] = Task.effect(ds.write.options(options.toMap).parquet(path))
+  def parquet(path: String): Task[Unit] = Task(ds.write.options(options.toMap).parquet(path))
 
-  def cache: Task[Unit] = Task.effect(ds.cache)
+  def cache: Task[Unit] = Task(ds.cache)
+
+  def format(name: String): Write[T] = this.copy(format = Some(name))
+}
+
+object Write {
+  def apply[T](ds: Dataset[T]): Write[T] = Write(ds, Nil, None)
 }
 
 trait SparkEnv {
@@ -25,7 +31,7 @@ trait SparkEnv {
 
   def read: Read
 
-  def write[T](ds: Dataset[T]): Write[T] = new Write(ds, Seq.empty)
+  def write[T](ds: Dataset[T]): Write[T] = Write(ds)
 
   trait Query {
     def sql(query: String): Task[DataFrame]
@@ -38,29 +44,30 @@ trait SparkEnv {
 
 class SparkZIO(spark: SparkSession) extends SparkEnv {
 
-  val readTrait: Read = new Read {
-    override def option(key: String, value: String): Read =
-      ???
+  private case class ReadImpl(ops: Seq[(String, String)]) extends Read {
+    private def dataFrameReader: DataFrameReader = ops.foldLeft(spark.read)({ case (dfr, (k, v)) => dfr.option(k, v) })
+
+    override def option(key: String, value: String): Read = copy(ops :+ ((key, value)))
 
     override def parquet(path: String): Task[DataFrame] =
-      Task.effect(spark.read.parquet(path))
+      Task(dataFrameReader.parquet(path))
 
     override def textFile(path: String): Task[DataFrame] =
-      Task.effect(spark.read.textFile(path).toDF())
+      Task(dataFrameReader.textFile(path).toDF())
   }
 
-  override def read: Read = readTrait
+  override def read: Read = ReadImpl(Nil)
 
-  override def write[T](ds: Dataset[T]): Write[T] = new Write(ds, Seq.empty)
+  override def write[T](ds: Dataset[T]): Write[T] = Write(ds)
 
   val queryTrait: Query = new Query {
     override def sql(query: String): Task[DataFrame] =
-      Task.effect(spark.sql(query))
+      Task(spark.sql(query))
   }
 
   override def query: Query = queryTrait
 
-  override def ss: Task[SparkSession] = Task.effect(spark)
+  override def ss: Task[SparkSession] = Task(spark)
 }
 
 object SparkEnv {
@@ -68,9 +75,9 @@ object SparkEnv {
   object implicits {
 
     implicit class DsOps[T](ds: Dataset[T]) {
-      def zcache: Task[Dataset[T]] = Task.effect(ds.cache)
+      def zcache: Task[Dataset[T]] = Task(ds.cache)
 
-      def zwrite: Write[T] = new Write(ds, Seq.empty)
+      def zwrite: Write[T] = Write(ds)
 
     }
 
@@ -83,27 +90,5 @@ object SparkEnv {
 
   def sparkSession: TaskS[SparkSession] = {
     ZIO.accessM(_.ss)
-  }
-}
-
-//TODO Move to Test
-object SparkTest {
-
-  def main(args: Array[String]): Unit = {
-
-    val runtime: DefaultRuntime = new DefaultRuntime {}
-    val sparkEnv                = new SparkZIO(SparkSession.builder.master("local[*]").getOrCreate())
-
-    import SparkEnv.implicits._
-
-    val programWrite: ZIO[SparkEnv, Throwable, DataFrame] = for {
-      df <- sparkEnv.read.textFile("tata")
-      _  <- df.zwrite.text("tata3")
-    } yield df
-
-    val liveProgramWrite: IO[Throwable, DataFrame] = programWrite.provide(sparkEnv)
-
-    val ya: DataFrame = runtime.unsafeRun(liveProgramWrite)
-    ya.show()
   }
 }
--- spark-zio/src/test/scala/io/univalence/sparkzio/SparkEnvImplicitClassTest.scala
+++ spark-zio/src/test/scala/io/univalence/sparkzio/SparkEnvImplicitClassTest.scala
@@ -1,12 +1,97 @@
 package io.univalence.sparkzio
 
-import org.apache.spark.sql.{ DataFrame, SparkSession }
+import io.univalence.sparkzio.SparkEnv.TaskS
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.FunSuite
-import scalaz.zio.{ DefaultRuntime, Task }
+import scalaz.zio.{DefaultRuntime, IO, Task, ZIO}
+import SparkEnv.implicits._
 
 class SparkEnvImplicitClassTest extends FunSuite {
   val runtime: DefaultRuntime = new DefaultRuntime {}
-  val ss: SparkSession        = SparkSession.builder.master("local[*]").getOrCreate()
-  val sparkEnv: SparkZIO      = new SparkZIO(ss)
+  val ss: SparkSession = SparkSession.builder.master("local[*]").getOrCreate()
+  val sparkEnv: SparkZIO = new SparkZIO(ss)
+  val pathToto: String = "spark-zio/src/test/resources/toto"
+
+  test("sparkEnv read and SparkEnv sql example") {
+
+    val prg: TaskS[(DataFrame, DataFrame)] = for {
+      ss <- SparkEnv.sparkSession
+      df <- sparkEnv.read.textFile(pathToto)
+      _ <- Task(df.createTempView("totoview"))
+      df2 <- SparkEnv.sql(s"""SELECT * FROM totoview""")
+      //_  <- df.zwrite.text("totoWriteZIO")
+    } yield (df, df2)
+
+    //Providing SparkEnv to ZIO
+    val liveProgram: IO[Throwable, (DataFrame, DataFrame)] = prg.provide(sparkEnv)
+
+    //Unsafe run
+    val resRun: (DataFrame, DataFrame) = runtime.unsafeRun(liveProgram)
+
+    assert(resRun._1.collect() sameElements resRun._2.collect())
+  }
+
+  test("ZIO catchAll example") {
+    val tigrou: String = "tigrou will only appear in success"
+
+    val prgSuccess: ZIO[Any, Throwable, Int] = for {
+      _ <- Task(println("Program with no Exception: "))
+      code <- Task {
+        for {
+          successCode <- Task(1)
+          _ <- Task(println(tigrou))
+        } yield successCode
+      }.flatten.catchAll {
+        case e: Exception => for {
+          _ <- Task {
+            print("Error: ");
+            println(e.getMessage)
+          }
+          errorCode <- Task(-1)
+        } yield errorCode
+      }
+    } yield code
+
+    val prgFail: ZIO[Any, Throwable, Int] = for {
+      _ <- Task(println("Program with Exception: "))
+      code <- Task {
+        for {
+          successCode <- Task(1 / 0)
+          _ <- Task(println(tigrou))
+        } yield successCode
+      }.flatten.catchAll {
+        case e: Exception => for {
+          _ <- Task {
+            print("Exception: ");
+            println(e.getMessage)
+          }
+          errorCode <- Task(-1)
+        } yield errorCode
+      }
+    } yield code
+
+    assert(runtime.unsafeRun(prgSuccess) == 1)
+    assert(runtime.unsafeRun(prgFail) == -1)
+  }
+
+  test("sometimes you will use collectAll") {
+    val primeSeq: Seq[Int] = Seq(2, 3, 5, 7, 11)
+
+    val prg: ZIO[SparkEnv, Throwable, Seq[ZIO[Any, Throwable, DataFrame]]] = for {
+      ss <- SparkEnv.sparkSession
+      seqDF <- {
+        import ss.implicits._
+        Task(
+          primeSeq.map {
+            primeNumber =>
+              for {
+                df <- sparkEnv.read.textFile(pathToto)
+              } yield df.withColumn("prime", 'primeNumber)
+          }
+        )
+      }
+    } yield seqDF
+
+  }
 
 }
