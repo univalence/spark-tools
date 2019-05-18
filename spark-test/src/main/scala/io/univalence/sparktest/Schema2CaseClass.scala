55556d5736ecfa4acd4d594126900ba229e7f5b1
Jonathan Winandy <jonathan@univalence.io>
2019-05-18 11:02:25 +0200

    [spark-tools] added a couple of todos in the code

--- spark-test/src/main/scala/io/univalence/sparktest/Schema2CaseClass.scala
+++ spark-test/src/main/scala/io/univalence/sparktest/Schema2CaseClass.scala
@@ -4,12 +4,12 @@ import org.apache.spark.sql.types._
 class Schema2CaseClass {
   type TypeConverter = DataType => String
 
-  def schemaToCaseClass(schema:StructType, className:String)(implicit tc:TypeConverter):String = {
-    def genField(s:StructField):String = {
+  def schemaToCaseClass(schema: StructType, className: String)(implicit tc: TypeConverter): String = {
+    def genField(s: StructField): String = {
       val f = tc(s.dataType)
       s match {
         case x if x.nullable => s"  ${s.name}:Option[$f]"
-        case _ => s"  ${s.name}:$f"
+        case _               => s"  ${s.name}:$f"
       }
     }
 
@@ -22,23 +22,23 @@ class Schema2CaseClass {
   }
 
   object implicits {
-    implicit val defaultTypeConverter:TypeConverter = (t:DataType) => { t match {
-      case _:ByteType => "Byte"
-      case _:ShortType => "Short"
-      case _:IntegerType => "Int"
-      case _:LongType => "Long"
-      case _:FloatType => "Float"
-      case _:DoubleType => "Double"
-      case _:DecimalType => "java.math.BigDecimal"
-      case _:StringType => "String"
-      case _:BinaryType => "Array[Byte]"
-      case _:BooleanType => "Boolean"
-      case _:TimestampType => "java.sql.Timestamp"
-      case _:DateType => "java.sql.Date"
-      case _:ArrayType => "scala.collection.Seq"
-      case _:MapType => "scala.collection.Map"
-      case _:StructType => "org.apache.spark.sql.Row"
-      case _ => "String"
-    }}
+    implicit val defaultTypeConverter: TypeConverter = {
+      case _: ByteType      => "Byte"
+      case _: ShortType     => "Short"
+      case _: IntegerType   => "Int"
+      case _: LongType      => "Long"
+      case _: FloatType     => "Float"
+      case _: DoubleType    => "Double"
+      case _: DecimalType   => "java.math.BigDecimal"
+      case _: StringType    => "String"
+      case _: BinaryType    => "Array[Byte]"
+      case _: BooleanType   => "Boolean"
+      case _: TimestampType => "java.sql.Timestamp"
+      case _: DateType      => "java.sql.Date"
+      case _: ArrayType     => "scala.collection.Seq"
+      case _: MapType       => "scala.collection.Map"
+      case _: StructType    => "org.apache.spark.sql.Row"
+      case _                => "String"
+    }
   }
 }
