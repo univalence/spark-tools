d94912d8f316187948aa6957d3242f6068cd8d73
hwki <philippe@univalence.io>
2019-07-11 15:06:03 +0200

    [plumbus] dumping cogroupDf here

--- plumbus/src/main/scala/io/univalence/plumbus/cogroup.scala
+++ plumbus/src/main/scala/io/univalence/plumbus/cogroup.scala
@@ -1,9 +1,13 @@
 package io.univalence.plumbus
 
-import org.apache.spark.rdd.{ PairRDDFunctions, RDD }
-import org.apache.spark.sql.{ Dataset, Encoder, KeyValueGroupedDataset }
+import org.apache.spark.Partitioner
+import org.apache.spark.rdd.{CoGroupedRDD, RDD}
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.{ArrayType, StructField}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, KeyValueGroupedDataset, Row, types}
 
 import scala.reflect.ClassTag
+import scala.util.Try
 
 object cogroup {
 
@@ -45,4 +49,38 @@ object cogroup {
           .map({ case (k, (ia, ib)) => (k, ia.toSeq, ib.toSeq) })
       )
       .toDS
+
+  def cogroupDf(group: DataFrame,
+                namedSubGroup: (String, DataFrame)*)
+               (byKey: String,
+                partitioner: Partitioner = Partitioner.defaultPartitioner(group.rdd, namedSubGroup.map(_._2.rdd): _*)
+               ): Try[DataFrame] =
+    Try {
+      val subGroup: Seq[DataFrame] = namedSubGroup.map(_._2)
+      val allFrames: Seq[DataFrame] = group +: subGroup
+      val allFramesKeyed: Seq[RDD[(String, Row)]] =
+        allFrames.map(df => {
+          val idx = df.columns.indexOf(byKey)
+          df.rdd.keyBy(_.get(idx).toString)
+        })
+
+      val cogroupRdd: CoGroupedRDD[String] = new CoGroupedRDD[String](allFramesKeyed, partitioner)
+
+      val rowRdd: RDD[Row] =
+        cogroupRdd.map(x => {
+          val rows: Array[Seq[Row]] = x._2.asInstanceOf[Array[Iterable[Row]]].map(_.toSeq)
+          val seq = rows.head.head.toSeq ++ rows.tail
+
+          new GenericRowWithSchema(seq.toArray, null).asInstanceOf[Row]
+        })
+
+      val schema =
+        types.StructType(
+          group.schema.fields
+            ++ namedSubGroup.map { case (name, df) => StructField(name, ArrayType(df.schema)) }
+        )
+
+      group.sparkSession.createDataFrame(rowRdd, schema)
+    }
+
 }
