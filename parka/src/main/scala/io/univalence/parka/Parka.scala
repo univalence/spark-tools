d25293f91bd59c4034a1c2134466d3e09063a426
Jonathan Winandy <jonathan@univalence.io>
2019-07-11 13:14:56 +0200

    [parka] major refactoring, lots of monoids

--- parka/src/main/scala/io/univalence/parka/Parka.scala
+++ parka/src/main/scala/io/univalence/parka/Parka.scala
@@ -1,20 +1,25 @@
 package io.univalence.parka
 
+import io.univalence.parka
+import io.univalence.parka.Delta.DeltaLong
 import io.univalence.parka.Describe.DescribeLong
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Dataset, Row}
 import io.univalence.sparktest.ValueComparison._
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 
 case class DatasetInfo(source: Seq[String], nStage: Long)
 
 case class Both[+T](left: T, right: T) {
-  def |>[U](f: (T, T) => U): U = f(left, right)
+  def fold[U](f: (T, T) => U): U = f(left, right)
+
+  def map[U](f: T => U): Both[U] = Both(f(left),f(right))
 }
 
 case class Outer(countRow: Both[Long], byColumn: Map[String,Both[Describe]])
 
 
-sealed trait Describe
+sealed trait Describe extends Serializable
 
 object Describe {
   case class DescribeLong(sum:Long) extends Describe
@@ -33,7 +38,7 @@ case class Inner(countRowEqual: Long,
 //case class ByColumn[T](columnName:String, value:T)
 
 
-trait Monoid[T] {
+trait Monoid[T] extends Serializable {
   def combine(x:T,y:T):T
   def empty:T
 }
@@ -75,11 +80,11 @@ object Monoid {
   def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = {
     new Typeclass[T] {
       override def combine(x: T, y: T): T = {
-        caseClass.construct(param =>
-          param.typeclass.combine(param.dereference(x), param.dereference(y)))
+        if(x == empty) y
+        else if(y == empty) x
+        else caseClass.construct(param => param.typeclass.combine(param.dereference(x), param.dereference(y)))
       }
-
-      override def empty: T = caseClass.construct(param => param.typeclass.empty)
+      override lazy val empty: T = caseClass.construct(param => param.typeclass.empty)
     }
   }
 
@@ -89,7 +94,7 @@ object Monoid {
 }
 
 
-sealed trait Delta {
+sealed trait Delta extends Serializable {
   def nEqual: Long
   def nNotEqual: Long
   def describe:Both[Describe]
@@ -102,17 +107,9 @@ object Delta {
                        error: Long) extends Delta
 
 
-
-
   implicit val deltaMonoid:Monoid[Delta] = Monoid.gen[DeltaLong].asInstanceOf[Monoid[Delta]]
 
 }
-
-
-
-
-
-
 
 
 
@@ -122,12 +119,82 @@ case class ParkaAnalysis(datasetInfo: Both[DatasetInfo], result:ParkaResult)
 
 object Parka {
 
-
-
-  val parkaResultM = Monoid.gen[ParkaResult]
-
-
   private val keyValueSeparator = "§"
+
+  def describe(row: Row)(keys:Set[String]):Map[String,Describe] = {
+    val fields = row.asInstanceOf[GenericRowWithSchema].schema.fieldNames
+
+    fields.filterNot(keys).map(name => {
+      val describe = row.getAs[Any](name) match {
+        case l:Long => DescribeLong(l)
+      }
+
+      name -> describe
+    }).toMap
+  }
+
+  def outer(row: Row, side: Side)(keys:Set[String]):Outer = {
+    Outer(
+      countRow = side match {
+          case Right => Both(left = 0, right = 1)
+          case Left =>  Both(left = 1, right = 0)
+        },
+      byColumn = describe(row)(keys).mapValues(d =>
+        side match {
+          case Right => Both(left = emptyDescribe, right = d)
+          case Left => Both(left = d, right = emptyDescribe)
+        }).map(identity) // oH No https://www.youtube.com/watch?v=P-3GOo_nWoc
+    )
+  }
+
+  def inner(left:Row, right: Row)(keys:Set[String]):Inner = {
+
+
+    val schema = left.asInstanceOf[GenericRowWithSchema].schema
+
+    val byNames: Map[String, Delta] = schema.fieldNames.filterNot(keys).map(name => {
+      val delta:Delta = (left.getAs[Any](name), right.getAs[Any](name)) match {
+        case (x:Long,y:Long) =>
+          if(x == y) {
+            val describe = DescribeLong(x)
+            DeltaLong(1, 0, Both(describe, describe), 0)
+          } else {
+            val diff = x - y
+            DeltaLong(0,1, Both(DescribeLong(x), DescribeLong(y)), diff * diff)
+          }
+      }
+      name -> delta
+    }).toMap
+
+    val isEqual = byNames.forall(_._2.nEqual == 1)
+    val nDiff = if(isEqual) 0 else byNames.count(_._2.nNotEqual == 1)
+    Inner(if(isEqual) 1 else 0, if(isEqual) 0 else 1, Map(nDiff -> 1), byNames)
+  }
+
+  private val emptyInner: Inner = Monoid.gen[Inner].empty
+  private val emptyOuter: Outer = Monoid.gen[Outer].empty
+  private val emptyDescribe: Describe = implicitly[Monoid[Describe]].empty
+
+  private val emptyResult:ParkaResult = Monoid.gen[ParkaResult].empty
+
+
+
+  def result(left:Iterable[Row],right:Iterable[Row])(keys:Set[String]):ParkaResult = {
+    (left, right) match {
+        //Only  Right
+      case (l,r) if l.isEmpty && r.nonEmpty => ParkaResult(emptyInner,outer(r.head,Right)(keys))
+        //Only Left
+      case (l,r) if l.nonEmpty && r.isEmpty => ParkaResult(emptyInner,outer(l.head,Left)(keys))
+      case (l,r) if l.nonEmpty && r.nonEmpty => ParkaResult(inner(l.head,r.head)(keys),emptyOuter)
+    }
+
+
+  }
+
+  private val monoidParkaResult = Monoid.gen[ParkaResult]
+  def combine(left:ParkaResult,right: ParkaResult): ParkaResult = {
+    monoidParkaResult.combine(left,right)
+  }
 
   def apply(leftDs: Dataset[_], rightDs: Dataset[_])(keyNames: String*): ParkaAnalysis = {
     assert(keyNames.nonEmpty, "you must have at least one key")
@@ -136,78 +203,34 @@ object Parka {
     import org.apache.spark.sql.functions._
     import leftDs.sparkSession.implicits._
 
+    val schema = leftDs.schema
+
     val keyValue: Row => String = r => keyNames.map(r.getAs[Any]).mkString(keyValueSeparator)
+val keys = keyNames.toSet
 
     val leftAndRight: RDD[(String, (Iterable[Row], Iterable[Row]))] =
       leftDs.toDF.rdd.keyBy(keyValue).cogroup(rightDs.toDF.rdd.keyBy(keyValue))
 
-    val diffs: RDD[RowComparison] = leftAndRight.map {
-      case (key, (left: Iterable[Row], right: Iterable[Row])) =>
-        // remove key from left and right (we dont compare key since they are equals)
-        (left, right) match {
-          case (l, r) if l.isEmpty => Alone(key, Right, r.head)
-          case (l, r) if r.isEmpty => Alone(key, Left, l.head)
-          case (l, r) =>
-            val lRow: Row = l.head
-            val rRow: Row = r.head
-
-            val differences = compareRow(lRow, rRow)
-
-            Crowd(key, differences, Both(lRow, rRow))
-        }
-    }
+    val res = leftAndRight.map({
+      case (k, (left, right)) => result(left,right)(keys)
+    }).reduce(combine)
 
 
 
 
-    val (countRow, countEq, countNotEq, allDifferences) =
-      diffs.aggregate((Both(0L, 0L), 0L, 0L, Seq.empty[Seq[Difference]]))(
-        {
-          case ((Both(l: Long, r: Long), countEq: Long, countNotEq: Long, allDifferences: Seq[Difference]),
-          diff) =>
-            diff match {
-              case Alone(_, Right, _) => (Both(l, r + 1), countEq, countNotEq, allDifferences)
-              case Alone(_, Left, _)  => (Both(l + 1, r), countEq, countNotEq, allDifferences)
-              case Crowd(_, differences: Seq[Difference], _) if differences != Seq.empty =>
-                (Both(l, r), countEq, countNotEq + 1, differences +: allDifferences)
-              case Crowd(_, _, _) => (Both(l, r), countEq + 1, countNotEq, allDifferences)
-            }
-        }, {
-          case ((Both(ll, lr), lcountEq, lcountNotEq, lallDifferences),
-          (Both(rl, rr), rcountEq, rcountNotEq, rallDifferences)) =>
-            (Both(ll + rl, lr + rr), lcountEq + rcountEq, lcountNotEq + rcountNotEq, lallDifferences ++ rallDifferences)
-        }
-      )
-
-    val differences = allDifferences.flatten.map{
-      case LongDifference(key, value) => (key, value)
-      case _ => ("error", 0)
-    }.groupBy(_._1).map { case (k, v) => k -> v.map { _._2}} //groupbykey made in scala
-
-    val contentNames = leftDs.columns.filter(!keyNames.contains(_)).toList
-
-    println(differences)
-
-    val datasetInfo = bothDatasetInfo(leftDs, rightDs)
-    val deltaInfo   = Inner(countEq, countNotEq, Map.empty, Map.empty)
-    val outerInfo   = Outer(countRow, Map.empty)
-
-    ParkaAnalysis(datasetInfo, ParkaResult(deltaInfo, outerInfo))
+    ParkaAnalysis(datasetInfo = Both(leftDs, rightDs).map(datasetInfo),
+      result = res)
   }
 
-  def datasetInfo(ds: Dataset[_]): DatasetInfo =
-    DatasetInfo(ds.collect().map(_.toString), 0L)
+  def datasetInfo(ds: Dataset[_]): DatasetInfo = DatasetInfo(Nil, 0L)
 
-  def bothDatasetInfo(leftDs: Dataset[_], rightDs: Dataset[_]): Both[DatasetInfo] =
-    Both(datasetInfo(leftDs), datasetInfo(rightDs))
 
-  def newOuterInfo(leftDs: Dataset[_], rightDs: Dataset[_])(keyNames: String*): Outer =
-    Outer(Both(leftDs.count, rightDs.count), ???)
 
   sealed trait Side
   case object Left extends Side
   case object Right extends Side
 
+  /*
   sealed trait RowComparison
   case class Alone(key: String, side: Side, row: Row) extends RowComparison
   case class Crowd(key: String, differences: Seq[Difference], rows: Both[Row]) extends RowComparison
@@ -216,10 +239,6 @@ object Parka {
   case class LongDifference(colname: String, difference: Long) extends Difference
   case class UnknowDifference(om: ObjectModification) extends Difference
 
-  def compareRow(left: Row, right: Row): Seq[Difference] = { //left != right
-    val differences = compareValue(fromRow(left), fromRow(right))
-    differences.map(exploitObjectModification)
-  }
 
   def exploitObjectModification(om: ObjectModification): Difference = om match {
     case ObjectModification(path, AddValue(AtomicValue(v))) =>
@@ -230,6 +249,8 @@ object Parka {
       LongDifference(path.firstName, rv.asInstanceOf[Number].longValue - lv.asInstanceOf[Number].longValue)
     case _ => UnknowDifference(om)
   }
+  */
+
 
   //
 }
