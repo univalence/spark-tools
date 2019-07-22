9ed47b89a349a68f5fe4913fa887b5d23aa39ba8
François Sarradin <fsarradin@gmail.com>
2019-07-22 15:17:52 +0200

    renaming + clean

--- parka/src/main/scala/io/univalence/parka/Histogram.scala
+++ parka/src/main/scala/io/univalence/parka/Histogram.scala
@@ -2,24 +2,34 @@ package io.univalence.parka
 
 import com.twitter.algebird.QTree
 
-case class Histogram(neg: Option[QTree[Unit]], countZero: Long, pos: Option[QTree[Unit]]) {
-  def count: Long = pos.map(_.count).getOrElse(0L) + countZero + neg.map(_.count).getOrElse(0L)
+case class Histogram(negatives: Option[QTree[Unit]], countZero: Long, positives: Option[QTree[Unit]]) {
+  lazy val positivesCount: Long = positives.map(_.count).getOrElse(0L)
+  lazy val negativesCount: Long = negatives.map(_.count).getOrElse(0L)
+  lazy val count: Long          = positivesCount + countZero + negativesCount
 
-  def quantileBounds(p: Double): (Double, Double) = {
-    require(p >= 0.0 && p <= 1.0, "The given percentile must be of the form 0 <= p <= 1.0")
-    val portion = p * count
+  lazy val min: Double = foldToSeq(x => -x.quantileBounds(1)._2,
+    x => if (x > 0) Some(0.0) else None,
+    x => x.quantileBounds(0)._1)(Math.min, Double.NegativeInfinity)
 
-    val p1: Long = neg.map(_.count).getOrElse(0L)
-    val p2: Long = p1 + countZero
+  lazy val max: Double = foldToSeq(x => -x.quantileBounds(0)._1,
+    x => if (x > 0) Some(0.0) else None,
+    x => x.quantileBounds(1)._2)(Math.max, Double.PositiveInfinity)
 
-    if (portion <= p1 && neg.isDefined) {
-      val (l, u) = neg.get.quantileBounds(1 - (portion / p1))
+
+  def quantileBounds(percentile: Double): (Double, Double) = {
+    require(percentile >= 0.0 && percentile <= 1.0, "The given percentile must be of the form 0 <= p <= 1.0")
+    val portion = percentile * count
+
+    val p2: Long = negativesCount + countZero
+
+    if (portion <= negativesCount && negatives.isDefined) {
+      val (l, u) = negatives.get.quantileBounds(1 - (portion / negativesCount))
       (-u, -l)
     } else if (portion <= p2) {
       (0.0, 0.0)
     } else {
       val portionPos: Double = portion - p2
-      pos.get.quantileBounds(portionPos / pos.get.count)
+      positives.get.quantileBounds(portionPos / positives.get.count)
     }
   }
 
@@ -31,14 +41,7 @@ case class Histogram(neg: Option[QTree[Unit]], countZero: Long, pos: Option[QTre
   private def foldToSeq[R](negT: QTree[Unit] => R,
                            countZeroT: Long  => Option[R],
                            posT: QTree[Unit] => R)(combine: (R, R) => R, empty: R): R =
-    Seq(neg.map(negT), countZeroT(countZero), pos.map(posT)).flatten.reduceOption(combine).getOrElse(empty)
-
-  lazy val min: Double = foldToSeq(x => -x.quantileBounds(1)._2,
-                                   x => if (x > 0) Some(0.0) else None,
-                                   x => x.quantileBounds(0)._1)(Math.min, Double.NegativeInfinity)
-  lazy val max: Double = foldToSeq(x => -x.quantileBounds(0)._1,
-                                   x => if (x > 0) Some(0.0) else None,
-                                   x => x.quantileBounds(1)._2)(Math.max, Double.PositiveInfinity)
+    Seq(negatives.map(negT), countZeroT(countZero), positives.map(posT)).flatten.reduceOption(combine).getOrElse(empty)
 
   private def approxCountBetween(lower: Double, upper: Double): Double =
     foldToSeq[Double](
@@ -47,6 +50,7 @@ case class Histogram(neg: Option[QTree[Unit]], countZero: Long, pos: Option[QTre
           val l: Double = Math.max(-upper, 0)
           val u: Double = -lower
           val (b1, b2)  = neg.rangeCountBounds(l, u)
+
           (b1 + b2) / 2
         } else 0.0
       },
@@ -55,6 +59,7 @@ case class Histogram(neg: Option[QTree[Unit]], countZero: Long, pos: Option[QTre
         if (upper > 0.0) {
           val l: Double = Math.max(lower, 0)
           val (b1, b2)  = pos.rangeCountBounds(l, upper)
+
           (b1 + b2) / 2
         } else 0.0
       }
@@ -64,18 +69,21 @@ case class Histogram(neg: Option[QTree[Unit]], countZero: Long, pos: Option[QTre
 
   def bin(n: Int): Seq[Bin] = {
     require(n >= 2, "at least 2 bins")
+
     val step = (max - min) / (n - 1)
-    (0 until n).map(i => {
+
+    (0 until n).map { i =>
       val lower = min + step * i - (step / 2)
       val upper = lower + step
+
       Bin((lower + upper) / 2, approxCountBetween(lower, upper).toLong)
-    })
+    }
   }
 
 }
 
 object Histogram {
-  def empty: Histogram = Histogram(neg = None, countZero = 0, pos = None)
+  def empty: Histogram = Histogram(negatives = None, countZero = 0, positives = None)
 
   def value(x: Long): Histogram =
     if (x == 0)
@@ -85,16 +93,16 @@ object Histogram {
     else if (x > Long.MaxValue - 2)
       value(Long.MaxValue - 2)
     else if (x < 0)
-      Histogram(neg = Some(QTree.value(-x)), countZero = 0, pos = None)
+      Histogram(negatives = Some(QTree.value(-x)), countZero = 0, positives = None)
     else
-      Histogram(neg = None, countZero = 0, pos = Some(QTree.value(x)))
+      Histogram(negatives = None, countZero = 0, positives = Some(QTree.value(x)))
 
   def value(x: Double): Histogram =
     if (x == 0)
       Histogram(None, 1, None)
     else if (x < 0)
-      Histogram(neg = Some(QTree.value(-x)), countZero = 0, pos = None)
+      Histogram(negatives = Some(QTree.value(-x)), countZero = 0, positives = None)
     else
-      Histogram(neg = None, countZero = 0, pos = Some(QTree.value(x)))
+      Histogram(negatives = None, countZero = 0, positives = Some(QTree.value(x)))
 
 }
--- parka/src/test/scala/io/univalence/parka/HistogramTest.scala
+++ parka/src/test/scala/io/univalence/parka/HistogramTest.scala
@@ -1,10 +1,12 @@
 package io.univalence.parka
 
 import io.univalence.parka.MonoidGen._
-import org.scalatest.prop.PropertyChecks
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import org.scalatest.FunSuite
 
-class HistogramTest extends FunSuite with PropertyChecks {
+class HistogramTest extends FunSuite with ScalaCheckPropertyChecks {
+
+  val histogramMonoid = MonoidGen.gen[Histogram]
 
   test("work with MinValue and MaxValue") {
     Histogram.value(Long.MinValue)
@@ -14,18 +16,18 @@ class HistogramTest extends FunSuite with PropertyChecks {
   def invariant(xs: Seq[Long]): Unit =
     if (xs.distinct.size >= 2 && xs.forall(x => (x < Long.MaxValue - 2) && (x > Long.MinValue + 2))) {
       val ys = xs.sorted
-      import MonoidGen._
-      val histo = MonoidGen.gen[Histogram].combineAll(xs.view.map(Histogram.value))
+      val histo = histogramMonoid.combineAll(xs.view.map(Histogram.value))
+
       assert(histo.count == ys.length)
 
       ys.zipWithIndex.foreach({
         case (value, index) =>
           val d      = index.toDouble / (ys.size - 1)
           val (l, u) = histo.quantileBounds(d)
+
           assert(value >= l)
           assert(value <= u)
       })
-
     }
 
   test("hello") {
@@ -33,10 +35,10 @@ class HistogramTest extends FunSuite with PropertyChecks {
   }
 
   test("bins") {
-
-    val histo = (22 to 42).map(x => Histogram.value(x.toDouble)).reduce(gen[Histogram].combine)
+    val histo = (22 to 42).map(x => Histogram.value(x.toDouble)).reduce(histogramMonoid.combine)
 
     val bins = histo.bin(6)
+
     assert(bins.size == 6)
     assert(bins.head.pos == histo.min)
     assert(bins.last.pos == histo.max)
