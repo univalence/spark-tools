d611926a273e643ea89b89a0543596f4bd6d277f
Jonathan Winandy <jonathan@univalence.io>
2019-07-30 08:43:31 +0200

    [parka] fix Delta Property Based Test on empty sequence

--- parka/src/test/scala/io/univalence/parka/DeltaTest.scala
+++ parka/src/test/scala/io/univalence/parka/DeltaTest.scala
@@ -7,28 +7,29 @@ class DeltaTest extends FunSuite with ScalaCheckPropertyChecks {
 
   test("testApply") {
     forAll((xs: Seq[Boolean]) => {
-      val (left, right)               = xs.splitAt(xs.size / 2)
-      val bb: Seq[(Boolean, Boolean)] = left.zip(right)
+      if (xs.size >= 2) {
+        val (left, right)               = xs.splitAt(xs.size / 2)
+        val bb: Seq[(Boolean, Boolean)] = left.zip(right)
 
-      import MonoidGen._
-      val monoid = gen[Delta]
+        import MonoidGen._
+        val monoid = gen[Delta]
 
-      val delta: Delta = monoid.combineAll(bb.map({ case (l, r) => Delta(l, r) }))
+        val delta: Delta = monoid.combineAll(bb.map({ case (l, r) => Delta(l, r) }))
 
-      assert(delta.nEqual == bb.count({ case (l, r)    => l == r }))
-      assert(delta.nNotEqual == bb.count({ case (l, r) => l != r }))
+        assert(delta.nEqual == bb.count({ case (l, r)    => l == r }))
+        assert(delta.nNotEqual == bb.count({ case (l, r) => l != r }))
 
-      def count(left: Boolean, right: Boolean): Long = bb.count(_ == ((left, right)))
+        def count(left: Boolean, right: Boolean): Long = bb.count(_ == ((left, right)))
 
-      val deltaBoolean = delta.asBoolean.get
+        val deltaBoolean = delta.asBoolean.get
 
-      import deltaBoolean._
+        import deltaBoolean._
 
-      assert(ft == count(left = false, right = true))
-      assert(tf == count(left = true, right  = false))
-      assert(tt == count(left = true, right  = true))
-      assert(ff == count(left = false, right = false))
-
+        assert(ft == count(left = false, right = true))
+        assert(tf == count(left = true, right  = false))
+        assert(tt == count(left = true, right  = true))
+        assert(ff == count(left = false, right = false))
+      }
     })
 
   }
